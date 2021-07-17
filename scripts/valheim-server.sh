5d327f550f80046af140ac5b0facde5b8dd3d937
Kristian Andre Aspevik <kristian.aspevik@gmail.com>
2021-07-18 00:36:33 +0100

    Pull server password from AWS SM

--- scripts/valheim-server.sh
+++ scripts/valheim-server.sh
@@ -10,8 +10,16 @@ valheimInstallPath="${valheimInstallPath:-/home/steam/valheim-install}"
 # Allow for us to run the server on any port, but default to 2456 (which is the expected port on client-side).
 serverPort=${serverPort:-2456}
 
-if [ -z "${serverDisplayName}" ] || [ -z "${serverWorldName}" ] || [ -z "${serverPassword}" ]; then
-  log_error "Expected serverDisplayName, serverWorldName and serverPassword environment variable to be set, but not found."
+if [ -z "${serverDisplayName}" ] || [ -z "${serverWorldName}" ]; then
+  log_error "Expected serverDisplayName, serverWorldName variable to be set, but not found."
+fi
+
+# Separate by serverWorldName so that we this script works with multiple servers.
+awsSecretId="/valheim/${serverWorldName}/serverPassword"
+serverPassword=$(aws secretsmanager get-secret-value --secret-id "${awsSecretId}" | jq -r .SecretString)
+
+if [ $? -ne 0 ]; then
+  log_error "Failed to pull server password from AWS SecretsManager with secret-id '${awsSecretId}'"
 fi
 
 # Install the Valheim app
